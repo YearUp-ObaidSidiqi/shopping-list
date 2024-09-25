e9e8b2134ae3b15e481e1c352d67df7a3a304061
SidiqiObaid <obaid.yearuo@gmail.com>
2024-09-25 15:10:04 -0400

    shopping list added

---
+++ src/main/java/com/pluralsight/ShopingList.java
@@ -0,0 +1,16 @@
+package com.pluralsight;
+
+public class ShopingList {
+
+    public static void main (String [] args ){
+
+        System.out.println("Shopping List: ");
+        System.out.println("-------------------------------------------------------");
+        System.out.println("Apple");
+        System.out.println("Kebab");
+        System.out.println("Lime");
+        System.out.println("watter");
+        System.out.println("blackpaper");
+
+    }
+}
